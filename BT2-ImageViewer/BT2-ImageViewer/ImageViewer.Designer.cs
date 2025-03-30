36b80830a02a5484f73671eabc5cfd2e5a8ace00
lazzerex <nahmsix2005@gmail.com>
2025-03-30 16:18:00 +0700

    Add files via upload

---
+++ BT2-ImageViewer/BT2-ImageViewer/ImageViewer.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace BT2_ImageViewer
+{
+    partial class imageViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadFolderButton = new System.Windows.Forms.Button();
+            this.imageFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.imagePictureBox = new System.Windows.Forms.PictureBox();
+            this.pathLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadFolderButton
+            // 
+            this.loadFolderButton.Location = new System.Drawing.Point(8, 21);
+            this.loadFolderButton.Name = "loadFolderButton";
+            this.loadFolderButton.Size = new System.Drawing.Size(116, 28);
+            this.loadFolderButton.TabIndex = 0;
+            this.loadFolderButton.Text = "&Load Folder";
+            this.loadFolderButton.UseVisualStyleBackColor = true;
+            this.loadFolderButton.Click += new System.EventHandler(this.loadFolderButton_Click);
+            // 
+            // imageFlowLayoutPanel
+            // 
+            this.imageFlowLayoutPanel.Location = new System.Drawing.Point(4, 278);
+            this.imageFlowLayoutPanel.Name = "imageFlowLayoutPanel";
+            this.imageFlowLayoutPanel.Size = new System.Drawing.Size(679, 104);
+            this.imageFlowLayoutPanel.TabIndex = 1;
+            this.imageFlowLayoutPanel.WrapContents = false;
+            // 
+            // imagePictureBox
+            // 
+            this.imagePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imagePictureBox.Location = new System.Drawing.Point(0, 70);
+            this.imagePictureBox.Name = "imagePictureBox";
+            this.imagePictureBox.Size = new System.Drawing.Size(799, 204);
+            this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagePictureBox.TabIndex = 2;
+            this.imagePictureBox.TabStop = false;
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(7, 424);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(55, 16);
+            this.pathLabel.TabIndex = 3;
+            this.pathLabel.Text = "                ";
+            // 
+            // imageViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.imagePictureBox);
+            this.Controls.Add(this.imageFlowLayoutPanel);
+            this.Controls.Add(this.loadFolderButton);
+            this.Name = "imageViewerForm";
+            this.Text = "Image Viewer";
+            
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadFolderButton;
+        private System.Windows.Forms.FlowLayoutPanel imageFlowLayoutPanel;
+        private System.Windows.Forms.PictureBox imagePictureBox;
+        private System.Windows.Forms.Label pathLabel;
+    }
+}
+
---
+++ BT2-ImageViewer/BT2-ImageViewer/ImageViewer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT2_ImageViewer
+{
+    public partial class imageViewerForm : Form
+    {
+        public imageViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loadFolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                var path = dialog.SelectedPath;
+                string[] files = Directory.GetFiles(path);
+
+                
+                imageFlowLayoutPanel.Controls.Clear();
+
+                foreach (var file in files)
+                {
+                    if (file.EndsWith(".jpg") || file.EndsWith(".png"))
+                    {
+                        PictureBox pictureBox = new PictureBox();
+                        pictureBox.Cursor = Cursors.Hand;
+                        pictureBox.Load(file);
+                        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pictureBox.Tag = file;
+
+                       
+                        pictureBox.Width = 150;
+                        pictureBox.Height = 100;
+
+                        pictureBox.Click += PictureBox_Click;
+
+                        imageFlowLayoutPanel.Controls.Add(pictureBox);
+                    }
+                }
+            }
+
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            if (sender is PictureBox pictureBox)
+            {
+                string imgPath = pictureBox.Tag.ToString();
+                imagePictureBox.Load(imgPath);
+                imagePictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pathLabel.Text = "File: " + imgPath + " is loaded.";
+            }
+        }
+
+        
+    }
+}
